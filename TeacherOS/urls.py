1e9e3a5582614f1a836cfc7f60ba5896497e887d
Postmaster <Postmaster@automovilismoonline.com>
2017-05-23 03:17:46 -0700

    last commit

--- TeacherOS/urls.py
+++ TeacherOS/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import home
+from .views import home, login
 
 urlpatterns = [
     url(r'^$', home, name = 'home'),
+    url(r'login', login, name = 'login'),
 ]
